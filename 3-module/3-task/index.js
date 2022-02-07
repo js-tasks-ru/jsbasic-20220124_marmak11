97b0fa959687b9dc2da10dcd57022c0caecbe7f7
Анна Ткаченко <annatkachenko@MacBook-Pro-Anna.local>
2022-02-07 12:44:57 +0700

    3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
+camelize('background-color')
+camelize('list-style-image')
+camelize('-webkit-transition')
+
 function camelize(str) {
-  // ваш код...
+  let newArr = str.split("-")
+
+    for (let index = 1; index < newArr.length; index++) {
+      newArr[index] = newArr[index].charAt(0).toUpperCase() + newArr[index].slice(1)
+      console.log(newArr)
+    }
+    
+  return newArr.join("")
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let user =(users.filter(item => item.age <= age))
+  user =(user.map(item => `${item.name}, ${item.balance}`).join('\n'))
+  return user
 }
+showSalary(users, age)
