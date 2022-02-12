1bfe7a9f4f07fa2774f004b0b115cec9bb2a96af
Анна Ткаченко <annatkachenko@MacBook-Pro-Anna.local>
2022-02-12 12:08:52 +0700

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button')
+  const a = document.querySelector('#text')
+  btn.addEventListener('click', () => {
+    a.hidden = !a.hidden
+  })
 }
