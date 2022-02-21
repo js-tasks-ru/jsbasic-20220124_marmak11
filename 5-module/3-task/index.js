8086050b4f69bddcdcff4112d530102276d98c39
Анна Ткаченко <annatkachenko@MacBook-Pro-Anna.local>
2022-02-21 14:58:21 +0700

    Добрый день, очень странно проходят тесты, когда я принимал ширину картинки через offsetWidth тесты не проходили, пришлось выставить ширину 500

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  const container = document.querySelector('.container');
+  const track = document.querySelector('.carousel');
+  const items = document.querySelector('.carousel__inner');
+  const btnPrev = document.querySelector('.carousel__arrow_left');
+  const btnNext = document.querySelector('.carousel__arrow_right');
+  const itemWidth = 500
+  let position = 0
+  
+  btnPrev.style.display = 'none'
+  btnNext.addEventListener('click', () => {
+    items.style.transform = `translateX(-${position+itemWidth}px)`
+    btnPrev.style.display = 'flex'
+    if (position >=itemWidth*2) {
+      btnNext.style.display = 'none'
+      position+=itemWidth
+    }
+     else {
+      position+=itemWidth
+     }
+  })
+  
+  btnPrev.addEventListener('click', () => {
+    items.style.transform = `translateX(-${position-itemWidth}px)`
+    if (position <=0 ) {
+      btnPrev.style.display = 'none'
+    }
+     else {
+      position-=itemWidth
+     }
+  }) 
 }
